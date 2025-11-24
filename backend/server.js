6df3d8a1d854a80685a11021aa8f1057e1b2e37c
Suhani Garg <suhanigarg59@gmail.com>
2025-11-24 15:43:11 +0530

    Add CORS support to server configuration

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
